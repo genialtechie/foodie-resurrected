20ebc9f7fbdc6bd8dfd4f71eb73c3337d2c1d7e4
genialtechie <thehaleembello@gmail.com>
2022-12-15 16:23:04 -0500

    fixed infinite renders bug

--- src/context/StatusContext.jsx
+++ src/context/StatusContext.jsx
@@ -1,21 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const StatusContext = React.createContext();
 
 export const StatusProvider = ({ children }) => {
   const [status, setStatus] = useState('Closed');
 
-  // get the current date, day, and time
-  const date = new Date();
-  const day = date.getDay();
-  const hour = date.getHours();
+  useEffect(() => {
+    // get the current date, day, and time
+    const date = new Date();
+    const day = date.getDay();
+    const hour = date.getHours();
 
-  // Check the current day and time, and update the status accordingly
-  if ((day === 3 || day === 4 || day === 5) && hour >= 16 && hour < 22) {
-    setStatus('Open');
-  } else if (day === 0 && hour >= 12 && hour < 22) {
-    setStatus('Open');
-  }
+    // Check the current day and time, and update the status accordingly
+    if ((day === 3 || day === 4 || day === 5) && hour >= 16 && hour < 22) {
+      setStatus('Open');
+    } else if (day === 0 && hour >= 12 && hour < 22) {
+      setStatus('Open');
+    }
+  }, []);
 
   return (
     <StatusContext.Provider value={status}>{children}</StatusContext.Provider>
